7023e23e002cf461039722e11bd9535a7508eb01
Zeuss5 <70203399+Zeuss5@users.noreply.github.com>
2025-10-31 17:21:02 +0300

    Import vllm and load general plugins

--- lm_eval/models/vllm_causallms.py
+++ lm_eval/models/vllm_causallms.py
@@ -32,10 +32,12 @@ from lm_eval.utils import (
 
 try:
     import ray
+    import vllm
     from vllm import LLM, SamplingParams, TokensPrompt
     from vllm.lora.request import LoRARequest
     from vllm.transformers_utils.tokenizer import get_tokenizer
     from vllm.utils import get_open_port
+    vllm.plugins.load_general_plugins()
 
     if parse_version(version("vllm")) >= parse_version("0.8.3"):
         from vllm.entrypoints.chat_utils import resolve_hf_chat_template
